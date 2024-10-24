5069f109a33bb66a05aa18935301ac8f18d76455
laurinha1912 <laura.araujof19@gmail.com>
2024-10-23 21:43:47 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"laura.araujof19@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
